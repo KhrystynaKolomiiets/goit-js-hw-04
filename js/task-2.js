dd82c910e6ae9384468e9466b85ccedb4faa1a09
KhrystynaKolomiiets <kaulitz89996@gmail.com>
2025-04-27 17:26:49 +0200

    updatetask-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function calcAverageCalories(days) {
-    let totalCalories = 0;
-    for (const day of days) { 
           if (days.length === 0) {
         return 0;
     }
+    let totalCalories = 0;
+    for (const day of days) { 
         totalCalories += day.calories;   
     } 
     const avarageCalories = totalCalories / days.length;
